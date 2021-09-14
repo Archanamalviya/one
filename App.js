c967fe383552a2b6d0620f0bdabaeb2642b8f63e
Archana Malviya <89104298+Archanamalviya@users.noreply.github.com>
2021-09-14 04:05:31 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+//  import Page1 from './Component/Page1';
+import Page2 from './Component/Page2';
+
+
+function App() {
+  return (
+    <div className="App">
+   {/* <Page1/> */}
+
+     <Page2/> 
+  <custom/>
+    </div>
+  );
+}
+
+export default App;
